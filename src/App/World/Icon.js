c3ce1f60d7169fdd8b95d1e848bdcdce17e333cd
Bruce Jin <bruce@proactiveailab.com>
2024-09-10 01:34:57 -0400

    hello

---
+++ src/App/World/Icon.js
@@ -0,0 +1,40 @@
+import * as THREE from 'three';
+import App from '../App';
+
+export default class Icon {
+    constructor(portalMesh) {
+      this.app = new App();
+      this.portalMesh = portalMesh;
+      this.setupEventHandlers();
+    }
+
+    setupEventHandlers() {
+        window.addEventListener('click', (event) => this.handleMouseEvent(event, 'click'));
+    }
+
+    handleMouseEvent(event, type) {
+        let mouse = new THREE.Vector2();
+        mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+        mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    
+        let raycaster = new THREE.Raycaster();
+        raycaster.setFromCamera(mouse, this.app.camera.instance);
+    
+        const intersects = raycaster.intersectObjects([this.portalMesh]);
+        if (type === 'click' && intersects.length > 0) {
+            this.handleMeshClick();
+        }
+    }
+
+    handleMeshClick() {
+        if (this.portalMesh.name === 'linkedin') {
+            window.open('https://www.linkedin.com/in/chengyu-bruce-jin-a7a095256/');
+        } else if (this.portalMesh.name === 'github') {
+            window.open('https://github.com/bruceJin0801');
+        }else{
+            window.open('mailto:c54jin@uwaterloo.ca?subject=Reach out to Bruce Jin&body=Hi, ', '_blank');
+        }
+       
+
+    }
+}
---
+++ src/App/World/Portal.js
@@ -0,0 +1,60 @@
+import * as THREE from 'three';
+import App from '../App';
+import ModalManager from '../UI/ModalManager';
+import bubbleSound from '../../../static/sound/bubble.mp3';
+import geeseSound from '../../../static/sound/geese.mp3';
+import dingSound from '../../../static/sound/ding.mp3';
+import techSound from '../../../static/sound/tech.wav';
+import hornSound from '../../../static/sound/horn.wav';
+import metalSound from '../../../static/sound/metal.wav';
+export default class Portal {
+    constructor(portalMesh, modalInfo) {
+      this.app = new App();
+      this.portalMesh = portalMesh;
+      this.modalInfo = modalInfo;
+      this.ModalManager = new ModalManager();
+      this.flag = false;
+      this.bubbleSound = new Audio(bubbleSound);
+    }
+   
+
+    loop(){
+        this.character = this.app.world.character.instance;
+        if (this.character) {         
+            const portalPosition = new THREE.Vector3();
+            this.portalMesh.getWorldPosition(portalPosition);
+            const distance = this.character.position.distanceTo(portalPosition);
+            if (distance < 2.5) {
+                if (!this.flag) {
+                    if (this.modalInfo.title === "University of Waterloo") {
+                        this.geeseSound = new Audio(geeseSound);
+                        this.geeseSound.play();
+                    }
+                    if (this.modalInfo.title === "UKG") {
+                        this.dingSound = new Audio(dingSound);
+                        this.dingSound.play();
+                    }
+                    if (this.modalInfo.title === "Proactive AI Lab") {
+                        this.techSound = new Audio(techSound);
+                        this.techSound.play();
+                    }
+                    if (this.modalInfo.title === "Ford") {
+                        this.hornSound = new Audio(hornSound);
+                        this.hornSound.play();
+                    }
+                    if (this.modalInfo.title === "Blue Streak Electronics") {
+                        this.metalSound = new Audio(metalSound);
+                        this.metalSound.play();
+                    }
+                    if (this.modalInfo.title === "JD.com") {
+                        this.bubbleSound.play();
+                    }
+                    this.ModalManager.openModal(this.modalInfo.title, this.modalInfo.description);
+                }
+                this.flag = true;
+            }else{
+                this.flag = false;
+            }
+          }
+    }
+}
