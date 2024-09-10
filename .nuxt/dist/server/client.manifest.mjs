66dd1ece169b14feef5aefd39931322168e2d7cc
Bruce Jin <bruce@proactiveailab.com>
2024-09-10 02:00:15 -0400

    d

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,66 @@
+export default {
+  "node_modules/nuxt/dist/app/components/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "BdEVKTwD.js",
+    "name": "error-404",
+    "src": "node_modules/nuxt/dist/app/components/error-404.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": []
+  },
+  "error-404.DXySnQZL.css": {
+    "file": "error-404.DXySnQZL.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/nuxt/dist/app/components/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "CMe7prJz.js",
+    "name": "error-500",
+    "src": "node_modules/nuxt/dist/app/components/error-500.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": []
+  },
+  "error-500.CIkJDsQj.css": {
+    "file": "error-500.CIkJDsQj.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "CouHm0ri.js",
+    "name": "entry",
+    "src": "node_modules/nuxt/dist/app/entry.js",
+    "isEntry": true,
+    "dynamicImports": [
+      "node_modules/nuxt/dist/app/components/error-404.vue",
+      "node_modules/nuxt/dist/app/components/error-500.vue"
+    ],
+    "css": [
+      "entry.BRWiKlTd.css"
+    ],
+    "_globalCSS": true
+  },
+  "entry.BRWiKlTd.css": {
+    "file": "entry.BRWiKlTd.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  }
+}
