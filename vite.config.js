4b4992782f74d273feab7c421a8cd93ee93ff023
Bruce Jin <bruce@proactiveailab.com>
2024-09-16 14:37:05 -0400

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import topLevelAwait from "vite-plugin-top-level-await";
 export default {
     root: 'src/',
     publicDir: '../static/',
-    base: './',
+    base: "/Personal-Website/",
     plugins: [
         wasm(),
         topLevelAwait()
