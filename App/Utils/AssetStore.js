778269e9d296ba61f197c69ff4481107e2e294d3
Bruce Jin <bruce@proactiveailab.com>
2024-09-19 15:52:00 -0400

    d

--- App/Utils/AssetStore.js
+++ App/Utils/AssetStore.js
@@ -3,12 +3,12 @@ import { createStore } from "https://esm.sh/zustand/vanilla";
 const assetsToLoad = [
   {
     id:'avatar',
-    path:'/models/avatar.glb',
+    path:'/Personal-website/static/models/avatar.glb',
     type: 'model' 
   },
   {
     id:'environment',
-    path:'/models/environment.glb',
+    path:'/Personal-website/static/models/environment.glb',
     type: 'model' 
   }
 ];
