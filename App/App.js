2de86f7aca5dd0475a7b636a2d3b2b84f847d1ae
Bruce Jin <bruce@proactiveailab.com>
2024-09-19 01:19:58 -0400

    s

--- App/App.js
+++ App/App.js
@@ -1,4 +1,4 @@
-import * as THREE from '../node_modules/three/build/three.js'
+import * as THREE from '../three.module.js'
 // import * as THREE from "../node_modules/three/"
 import Camera from './Camera.js'
 import Renderer from './Renderer.js'
